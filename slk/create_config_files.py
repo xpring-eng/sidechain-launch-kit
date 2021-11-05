efa915235365ebc57d025a68cab98d5d7b1b88df
Mayukha Vadari <mvadari@ripple.com>
2021-11-05 13:07:57 -0400

    rename config_strs -> cfg_strs

--- slk/create_config_files.py
+++ slk/create_config_files.py
@@ -26,12 +26,8 @@ from typing import Dict, List, Optional
 from xrpl.models import IssuedCurrencyAmount
 
 from slk.common import eprint
+from slk.config.cfg_strs import generate_sidechain_stanza, get_cfg_str, get_ips_stanza
 from slk.config.config_params import ConfigParams
-from slk.config.config_strs import (
-    generate_sidechain_stanza,
-    get_cfg_str,
-    get_ips_stanza,
-)
 from slk.config.helper_classes import Ports, XChainAsset
 from slk.config.network import Network, SidechainNetwork
 
