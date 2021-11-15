e2b1ffc7af01ebc27b42797edb1132874af7da12
Mayukha Vadari <mvadari@ripple.com>
2021-11-15 18:19:35 -0500

    remove exe stuff from ConfigParams

--- slk/config/config_params.py
+++ slk/config/config_params.py
@@ -14,12 +14,6 @@ def _parse_args() -> argparse.Namespace:
     )
 
     parser.add_argument(
-        "--exe",
-        "-e",
-        help=("path to rippled executable"),
-    )
-
-    parser.add_argument(
         "--usd",
         "-u",
         action="store_true",
@@ -49,17 +43,6 @@ def _parse_args() -> argparse.Namespace:
 class ConfigParams:
     def __init__(self: ConfigParams) -> None:
         args = _parse_args()
-
-        if "RIPPLED_MAINCHAIN_EXE" in os.environ:
-            self.exe = os.environ["RIPPLED_MAINCHAIN_EXE"]
-        if args.exe:
-            self.exe = args.exe
-        # if `self.mainchain_exe` doesn't exist (done this way for typing purposes)
-        if not hasattr(self, "exe"):
-            raise Exception(
-                "Missing exe location. Either set the env variable "
-                "RIPPLED_MAINCHAIN_EXE or use the --exe_mainchain command line switch"
-            )
 
         if "RIPPLED_SIDECHAIN_CFG_DIR" in os.environ:
             self.configs_dir = os.environ["RIPPLED_SIDECHAIN_CFG_DIR"]
